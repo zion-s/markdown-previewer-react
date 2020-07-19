5f26a5f27c01041150456aecd2e6dc9a453d587c
Zion Sandeep <59429929+zion-s@users.noreply.github.com>
2020-07-19 20:31:13 +0530

    Bug fixes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Editor from './components/Editor';
+import Preview from './components/Preview';
+import Card from './components/Card/Card';
+//import Footer from './components/Footer/Footer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const initialMd = `# Hello! 
+## Welcome to my markdown previewer
+
+[Google](https://www.google.com/)
+
+Here is a list:
+- list element
+- list element
+
+this is a **bold** text, an _emphasis_ , a _**combined emphasis**_ , and a ~~strikethrough~~
+
+Head 1 | Head 2 | Head 3
+------ |--------| ------
+item1 | item2 | item3
+item4 | item5 | item6
+item7 | item8 | item9
+
+> example of blockquote
+
+Inline \`code\`
+
+\`\`\`
+// code block
+
+function sum (x, y) {
+  return x + y;
+}
+\`\`\`
+`;
+
+class App extends Component {
+  state = {
+    markdownValue: initialMd
+  };
+
+  handleChange = e => {
+    this.setState({
+      markdownValue: e.target.value
+    });
+  };
+
+  render() {
+    const { markdownValue } = this.state;
+
+    return (
+      <div className="App">
+        <h1 className="App-title">Markdown Previewer</h1>
+        <main className="App-content">
+          <Card title="Editor">
+            <Editor
+              onChange={this.handleChange}
+              markdownValue={markdownValue}
+            />
+          </Card>
+          <Card title="Preview">
+            <Preview markdownValue={markdownValue} />
+          </Card>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
